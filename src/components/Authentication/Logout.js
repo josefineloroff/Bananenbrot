ae8e0ac9835ea5f0980a135e4b15070db2b0e7f4
Josefine Loroff <josefine.loroff@studium.fernuni-hagen.de>
2018-06-28 11:17:22 +0200

    add styles

--- src/components/Authentication/Logout.js
+++ src/components/Authentication/Logout.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react'
 import './authentication.css'
 import { image } from '../../assets/images/Fehlermeldung.jpg'
 import MyNavbar from '../Navbar/MyNavbar'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import AppBar from 'material-ui/AppBar'
-import RaisedButton from 'material-ui/RaisedButton'
-import TextField from 'material-ui/TextField'
 
 export default class Logout extends Component {
   render() {
     return (
       <div>
         <MyNavbar />
-        <div className="logout">
-          <MuiThemeProvider>
-            <div>
-              <h2>Goodbye</h2>
-            </div>
-          </MuiThemeProvider>
+        <div className="row">
+          <br />
+          <br />
+          <div className="logo">
+            <img
+              src={require('../../assets/images/goodbye-clipart-black-and-white-16.jpg')}
+              style={{
+                height: '150px',
+                width: '250px',
+              }}
+            />
+          </div>
         </div>
       </div>
     )
